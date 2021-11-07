2710ff8be16552a819b0b7f3c15eeb010f02dc14
Anders Bennedsgaard <abbennedsgaard@gmail.com>
2021-11-07 12:30:05 +0100

    Rename packages.txt to apt-packages.txt

--- bin/install-packages.sh
+++ bin/install-packages.sh
@@ -6,7 +6,7 @@ SCRIPTFULLPATH=$(realpath $0)
 BASEDIR=$(dirname $SCRIPTFULLPATH)
 
 echo "Install using apt:"
-cat "$BASEDIR/../packages.txt" | xargs apt install -y
+cat "$BASEDIR/../apt-packages.txt" | xargs apt install -y
 
 echo "Install using snap:"
 snap install --classic code
