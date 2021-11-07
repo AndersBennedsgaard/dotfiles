003f1af1fb89e93abce8a2d520d7f795089578a7
Anders Bennedsgaard <abbennedsgaard@gmail.com>
2021-11-07 12:37:33 +0100

    Install Python packages

--- bin/install-packages.sh
+++ bin/install-packages.sh
@@ -5,8 +5,14 @@ set -e
 SCRIPTFULLPATH=$(realpath $0)
 BASEDIR=$(dirname "$SCRIPTFULLPATH")
 
+echo
 echo "Install using apt:"
 xargs apt install -y < "$BASEDIR/../apt-packages.txt"
 
+echo
 echo "Install using snap:"
 snap install --classic code
+
+echo
+echo "Install using pip:"
+pip3 install -r "$BASEDIR/../pip-packages.txt"
