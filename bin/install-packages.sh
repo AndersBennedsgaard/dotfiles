1b3421c1c6ac147ba792aedac08464501a8ed629
Anders Bennedsgaard <abbennedsgaard@gmail.com>
2021-11-07 12:06:50 +0100

    Install VSCode with snap

--- bin/install-packages.sh
+++ bin/install-packages.sh
@@ -5,4 +5,8 @@ set -e
 SCRIPTFULLPATH=$(realpath $0)
 BASEDIR=$(dirname $SCRIPTFULLPATH)
 
+echo "Install using apt:"
 cat "$BASEDIR/../packages.txt" | xargs apt install -y
+
+echo "Install using snap:"
+snap install --classic code
