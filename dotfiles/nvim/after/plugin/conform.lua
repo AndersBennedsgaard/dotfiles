42ad5ef61c8417533f5216dd3066d54feed02b82
Anders Bennedsgaard <abbennedsgaard@gmail.com>
2025-08-10 13:47:14 +0200

    feat: prettier for html and css formatting

--- dotfiles/nvim/after/plugin/conform.lua
+++ dotfiles/nvim/after/plugin/conform.lua
@@ -10,6 +10,8 @@ require("conform").setup({
     python = { "ruff_format" },
     javascript = { "prettierd", "prettier", stop_after_first = true },
     typescript = { "prettierd", "prettier", stop_after_first = true },
+    html = { "prettierd", "prettier", stop_after_first = true },
+    css = { "prettierd", "prettier", stop_after_first = true },
     -- Conform can also run multiple formatters sequentially
     -- python = { "isort", "black" },
     --
