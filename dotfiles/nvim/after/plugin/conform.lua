43d312e638b2aa07da6ebb0e16caeaf2acb4f9dc
Anders Bjørnkjær Bennedsgaard <abbennedsgaard@gmail.com>
2026-02-04 08:23:35 +0100

    feat: use prettier for yaml formatting

--- dotfiles/nvim/after/plugin/conform.lua
+++ dotfiles/nvim/after/plugin/conform.lua
@@ -4,6 +4,8 @@ require("conform").setup({
     timeout_ms = 500,
     lsp_fallback = true,
   },
+  formatters = {
+  },
   formatters_by_ft = {
     lua = { "stylua" },
     go = { "goimports" },
@@ -12,6 +14,7 @@ require("conform").setup({
     typescript = { "prettierd", "prettier", stop_after_first = true },
     html = { "prettierd", "prettier", stop_after_first = true },
     css = { "prettierd", "prettier", stop_after_first = true },
+    yaml = { "prettierd", "prettier", stop_after_first = true },
     -- Conform can also run multiple formatters sequentially
     -- python = { "isort", "black" },
     --
